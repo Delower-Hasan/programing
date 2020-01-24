f9a0dbe5ef198dfef7023c8d8609013215500945
Delower Hasan <dhhasansaha11@gmail.com>
2020-01-24 22:32:26 +0600

    image upload, update and delete in laravel code

---
+++ laravel/imgUpload.php
@@ -0,0 +1,29 @@
+<!-- insertion image -->
+if($request->hasFile('logo')){
+    $get_image = $request->file('logo');
+    $image = Str::random(10).".".$get_image->getClientOriginalExtension();
+    Image::make($get_image)->resize(300, 200)->save(public_path('/backend/upload_image/'.$image));
+}
+else{
+    $image = 'default.png';
+}
+
+<!-- update image -->
+if( $request->hasFile('logo')){
+    $get_image = $request->file('logo'); //orginal image;
+    $image = Str::random(5).'.'.$get_image->getClientOriginalExtension(); //to get New(custom) image name
+    Image::make($get_image)->resize(300, 200)->save(public_path('/backend/upload_image/'.$image));
+    if($request->file("logo")){
+        $new_image = Header::findOrFail($id)->logo;
+        unlink($new_image);
+    }
+    }
+else{
+
+$image = 'default.png';
+}
+
+<!-- delete image -->
+$delete = Header::findOrFail($id)->logo;
+unlink($delete);
+Header::findOrFail($id)->delete();
